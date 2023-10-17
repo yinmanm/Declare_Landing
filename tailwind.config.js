ccc609146ac8a3a4b4e013dacba7f8b85bb7fa57
yinmanman <62935215+yinmanm@users.noreply.github.com>
2023-10-17 12:28:29 +0800

    font

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,9 +22,10 @@ module.exports = {
         '4xl': '2.5rem',
       },
       fontFamily: {
-        sans: ['Mona Sans', ...defaultTheme.fontFamily.sans],
+        sans: ['SuisseIntl', ...defaultTheme.fontFamily.sans],
+        suisseIntl: ['SuisseIntl', ...defaultTheme.fontFamily.sans],
         display: [
-          ['Mona Sans', ...defaultTheme.fontFamily.sans],
+          ['SuisseIntl', ...defaultTheme.fontFamily.sans],
           { fontVariationSettings: '"wdth" 125' },
         ],
       },
